e59eeb097aa0c43e1b84f93d181afa688ae6c9e3
OriginalCodes01 <156553999+OriginalCodes01@users.noreply.github.com>
2024-01-24 23:04:47 +0000

    ADDED THE TALENTS BUTTONS

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
+TALENTS = [
+  {
+    "id": 1,
+    "title": "Film making" 
+    },
+ {
+    "id": 1,
+    "title": "Oration" 
+  },
+{
+  "id": 2,
+  "title": "Video editing" 
+},
+{
+  "id": 3,
+  "title": "Programming" 
+},
+{
+  "id": 4,
+  "title": "Fashion" 
+},
+{
+  "id": 5,
+  "title": "Sweet man" 
+}
+         ]
 
 @app.route("/")
 def hello_world():
-  return render_template("home.html")
+  return render_template("home.html",
+                         Talents=TALENTS)
 
 
 if __name__ == "__main__":
